40fc48e5581289cb849a4e7cf555886bff6a52a2
Raju Rana <raju.rana@appsbee.com>
2017-03-28 16:40:02 +0530

    initial commit

---
+++ application/controllers/api/Auth.php
@@ -0,0 +1,445 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+// This can be removed if you use __autoload() in config.php OR use Modular Extensions
+/** @noinspection PhpIncludeInspection */
+require APPPATH . '/libraries/REST_Controller.php';
+
+// use namespace
+use Restserver\Libraries\REST_Controller;
+
+/**
+ * This is an example of a few basic user interaction methods you could use
+ * all done with a hardcoded array
+ *
+ * @package         CodeIgniter
+ * @subpackage      Rest Server
+ * @category        Controller
+ * @author          Phil Sturgeon, Chris Kacerguis
+ * @license         MIT
+ * @link            https://github.com/chriskacerguis/codeigniter-restserver
+ */
+class Auth extends REST_Controller {
+
+    function __construct()
+    {
+        // Construct the parent class
+        parent::__construct();
+
+        // Configure limits on our controller methods
+        // Ensure you have created the 'limits' table and enabled 'limits' within application/config/rest.php
+        /*$this->methods['users_get']['limit'] = 500; // 500 requests per hour per user/key
+        $this->methods['users_post']['limit'] = 100; // 100 requests per hour per user/key
+        $this->methods['users_delete']['limit'] = 50; // 50 requests per hour per user/key*/
+        $this->load->model('Account','',TRUE);
+    }
+
+    public function signup_post()
+    {
+        $data['fname'] = $this->input->post('fname');
+        $data['lname'] = $this->input->post('lname');
+        $data['email'] = $this->input->post('email');
+        $data['password'] = $this->input->post('password');
+        $is_social = $this->input->post('is_social');
+        $data['asocial'] = $this->input->post('signup_source');
+        $data['asocial_id'] = $this->input->post('social_id');
+
+
+        if($is_social == 1){
+            $accountData = array(
+                                array(
+                                    'key' => 'aemail',
+                                    'value' => $data['email'],
+                                    'escape' => true
+                                ),
+                                array(
+                                    'key' => 'asocial',
+                                    'value' => $data['asocial'],
+                                    'escape' => true
+                                ),
+                                array(
+                                    'key' => 'asocial_id',
+                                    'value' => $data['asocial_id'],
+                                    'escape' => true
+                                ),
+                                array(
+                                    'key' => 'afirstname',
+                                    'value' => $data['fname'],
+                                    'escape' => true
+                                ),
+                                array(
+                                    'key' => 'alastname',
+                                    'value' => $data['lname'],
+                                    'escape' => true
+                                ),
+                                array(
+                                    'key' => 'aspeed',
+                                    'value' => '25',
+                                    'escape' => true
+                                ),
+                                array(
+                                    'key' => 'acreated',
+                                    'value' => date('Y-m-d H:i:s'),
+                                    'escape' => true
+                                )
+                            );
+            if($this->Account->emailExists($data['email'])){
+                // Set the response and exit
+                $this->set_response([
+                    'status' => FALSE,
+                    'message' => 'Email already exists!',
+                    'response' => new stdClass()
+                ], REST_Controller::HTTP_OK); // NOT_FOUND (404) being the HTTP response code
+            }else{
+
+                $account_id = $this->Account->accountExist($data['asocial_id']);
+                if(!$account_id){
+                    $account_id = $this->Account->addAccount($accountData);
+                }
+                
+                if($account_id) {
+                    $accData = $this->Account->getAccount($account_id);
+                    $response = [
+                        'status' => TRUE,
+                        'message' => 'Successful!',
+                        'response' => $accData
+                    ];
+
+                    $this->set_response($response, REST_Controller::HTTP_CREATED); // CREATED (201) being the HTTP response code
+                }else{
+                    // Set the response and exit
+                    $this->response([
+                        'status' => FALSE,
+                        'message' => 'No users were found',
+                        'response' => new stdClass()
+                    ], REST_Controller::HTTP_OK); // NOT_FOUND (404) being the HTTP response code
+                }
+            }
+        }else{
+            $accountData = array(
+                                array(
+                                    'key' => 'aemail',
+                                    'value' => $data['email'],
+                                    'escape' => true
+                                ),
+                                array(
+                                    'key' => 'apassword',
+                                    'value' => md5($data['password']),
+                                    'escape' => true
+                                ),
+                                array(
+                                    'key' => 'afirstname',
+                                    'value' => $data['fname'],
+                                    'escape' => true
+                                ),
+                                array(
+                                    'key' => 'alastname',
+                                    'value' => $data['lname'],
+                                    'escape' => true
+                                ),
+                                array(
+                                    'key' => 'aspeed',
+                                    'value' => '25',
+                                    'escape' => true
+                                ),
+                                array(
+                                    'key' => 'acreated',
+                                    'value' => date('Y-m-d H:i:s'),
+                                    'escape' => true
+                                )
+                            );
+
+            if($this->Account->emailExists($data['email'])){
+                // Set the response and exit
+                $this->set_response([
+                    'status' => FALSE,
+                    'message' => 'Email already exists!',
+                    'response' => new stdClass()
+                ], REST_Controller::HTTP_OK); // NOT_FOUND (404) being the HTTP response code
+            }else{
+
+                $account_id = $this->Account->addAccount($accountData);
+
+                if($account_id) {
+                    $accData = $this->Account->getAccount($account_id);
+                    $response = [
+                        'status' => TRUE,
+                        'message' => 'Successful!',
+                        'response' => $accData
+                    ];
+
+                    $this->set_response($response, REST_Controller::HTTP_CREATED); // CREATED (201) being the HTTP response code
+                }else{
+                    // Set the response and exit
+                    $this->response([
+                        'status' => FALSE,
+                        'message' => 'No users were found',
+                        'response' => new stdClass()
+                    ], REST_Controller::HTTP_OK); // NOT_FOUND (404) being the HTTP response code
+                }
+            }
+        }
+    }
+
+    public function login_post()
+    {
+        $data['email'] = $this->input->post('email');
+        $data['password'] = $this->input->post('password');
+
+        if(!$this->Account->emailExists($data['email'])){
+            // Set the response and exit
+            $this->set_response([
+                'status' => FALSE,
+                'message' => 'No users were found with this Email id!',
+                'response' => new stdClass()
+            ], REST_Controller::HTTP_OK); // NOT_FOUND (404) being the HTTP response code
+        }else{
+
+            $accountData = $this->Account->accountLogin($data['email'],$data['password']);
+
+            if($accountData) {
+                $response = [
+                    'status' => TRUE,
+                    'message' => 'Successful!',
+                    'response' => $accountData
+                ];
+
+                $this->set_response($response, REST_Controller::HTTP_CREATED); // CREATED (201) being the HTTP response code
+            }else{
+                // Set the response and exit
+                $this->response([
+                    'status' => FALSE,
+                    'message' => 'Password not correct!',
+                    'response' => new stdClass()
+                ], REST_Controller::HTTP_OK); // NOT_FOUND (404) being the HTTP response code
+            }
+        }
+    }
+
+    public function getProfile_post()
+    {
+        $data['id'] = $this->input->post('accountId');
+        $accountData = $this->Account->getAccount($data['id']);
+
+        if(!$accountData){
+            // Set the response and exit
+            $this->set_response([
+                'status' => FALSE,
+                'message' => 'No users were found with this id!',
+                'response' => new stdClass()
+            ], REST_Controller::HTTP_OK); // NOT_FOUND (404) being the HTTP response code
+        }else{
+            $response = [
+                'status' => TRUE,
+                'message' => 'Successful!',
+                'response' => $accountData
+            ];
+
+            $this->set_response($response, REST_Controller::HTTP_CREATED); // CREATED (201) being the HTTP response code
+        }
+    }
+
+    public function changePassword_post()
+    {
+        $data['id'] = $this->input->post('accountId');
+        $data['old_password'] = $this->input->post('old_password');
+        $uData['apassword'] = $this->input->post('new_password');
+
+        $accountData = $this->Account->getAccountAuthInformation($data['id']);
+
+        if(!$accountData){
+            // Set the response and exit
+            $this->set_response([
+                'status' => FALSE,
+                'message' => 'No users were found with this id!',
+                'response' => new stdClass()
+            ], REST_Controller::HTTP_OK); // NOT_FOUND (404) being the HTTP response code
+        }else{
+            if(($accountData->apassword == "") && ($accountData->asocial != 'E')){
+                $logType = ($accountData->asocial == 'F')?'Facebook':'Twitter';
+                // Set the response and exit
+                $this->set_response([
+                    'status' => FALSE,
+                    'message' => 'You cannot change password as you have logged in with'.$logType.'.',
+                    'response' => new stdClass()
+                ], REST_Controller::HTTP_OK); // NOT_FOUND (404) being the HTTP response code
+            }else{
+                if( !empty($data['old_password']) && (md5($data['old_password']) == $accountData->apassword) ){
+                    if($this->Account->updateAccount($uData,$accountData->id)){
+                        $response = [
+                            'status' => TRUE,
+                            'message' => 'Password reset successful!',
+                            'response' => $accountData
+                        ];
+
+                        $this->set_response($response, REST_Controller::HTTP_CREATED); // CREATED (201) being the HTTP response code
+                    }else{
+                        // Set the response and exit
+                        $this->set_response([
+                            'status' => FALSE,
+                            'message' => 'Password reset unsuccessful! Please try again.',
+                            'response' => new stdClass()
+                        ], REST_Controller::HTTP_OK); // NOT_FOUND (404) being the HTTP response code
+                    }
+                }else{
+                    // Set the response and exit
+                    $this->set_response([
+                        'status' => FALSE,
+                        'message' => 'Old password does not match!',
+                        'response' => new stdClass()
+                    ], REST_Controller::HTTP_OK); // NOT_FOUND (404) being the HTTP response code
+                }
+            }
+        }
+    }
+
+    public function updateProfile_post(){
+
+        $data['id'] = $this->input->post('accountId');
+        $uData['afirstname'] = $this->input->post('fname');
+        $uData['alastname'] = $this->input->post('lname');
+        $uData['alocation'] = $this->input->post('location');
+
+        if (!empty($_FILES['photo']['name'])) {
+            $photoUploadPath = 'uploads/';
+            if (!file_exists($photoUploadPath))     
+                mkdir($photoUploadPath, 0777, true);
+
+            $this->load->library('upload');
+            $config = array();
+            $config['upload_path'] = $photoUploadPath;
+            $config['allowed_types'] = 'gif|jpg|png|jpeg';
+            $config['max_size'] = '1024000'; //10240
+            $config['max_width']  = '0';
+            $config['max_height']  = '0';
+            $config['file_name'] = bin2hex( mcrypt_create_iv( 32, MCRYPT_DEV_URANDOM ) );
+            $this->upload->initialize($config);
+            if ( ! $this->upload->do_upload('photo',$config) ){
+                // Set the response and exit
+                $this->set_response([
+                    'status' => FALSE,
+                    'message' => 'Profile image does not updated! Please try again.',
+                    'response' => $this->upload->display_errors()
+                ], REST_Controller::HTTP_OK); // NOT_FOUND (404) being the HTTP response code
+            }else{
+                $uphdata = array('upload_data' => $this->upload->data());
+                $uData['aprofile_photo'] = $uphdata['upload_data']['file_name'];
+            }
+        }
+
+        $accountData = $this->Account->updateAccount($uData,$data['id']);
+
+        if($accountData){
+            $response = [
+                'status' => TRUE,
+                'message' => 'Profile update successfully!',
+                'response' => new stdClass()
+            ];
+
+            $this->set_response($response, REST_Controller::HTTP_CREATED); // CREATED (201) being the HTTP response code
+        }else{
+            // Set the response and exit
+            $this->set_response([
+                'status' => FALSE,
+                'message' => 'Profile does not updated! Please try again.',
+                'response' => new stdClass()
+            ], REST_Controller::HTTP_OK); // NOT_FOUND (404) being the HTTP response code
+        }
+    }
+
+    public function setSpeed_post(){
+
+        $data['id'] = $this->input->post('accountId');
+        $uData['aspeed'] = $this->input->post('speed');
+        $accountData = $this->Account->updateAccount($uData,$data['id']);
+
+        if($accountData){
+            $response = [
+                'status' => TRUE,
+                'message' => 'Profile update successfully!',
+                'response' => new stdClass()
+            ];
+
+            $this->set_response($response, REST_Controller::HTTP_CREATED); // CREATED (201) being the HTTP response code
+        }else{
+            // Set the response and exit
+            $this->set_response([
+                'status' => FALSE,
+                'message' => 'Profile does not updated! Please try again.',
+                'response' => new stdClass()
+            ], REST_Controller::HTTP_OK); // NOT_FOUND (404) being the HTTP response code
+        }
+    }
+
+    public function getSpeed_post(){
+
+        $data['id'] = $this->input->post('accountId');
+        $accountData = $this->Account->getAccountField($data['id'],'aspeed');
+
+        if(!empty($accountData)){
+            $response = [
+                'status' => TRUE,
+                'message' => 'Profile speed fetched successfully!',
+                'response' => $accountData
+            ];
+
+            $this->set_response($response, REST_Controller::HTTP_CREATED); // CREATED (201) being the HTTP response code
+        }else{
+            // Set the response and exit
+            $this->set_response([
+                'status' => FALSE,
+                'message' => 'Profile speed does not found! Please try again.',
+                'response' => new stdClass()
+            ], REST_Controller::HTTP_OK);
+        }
+    }
+
+    public function forgotPassword_post() { 
+        $from_email = "no-reply@sad.com"; 
+        $account_email = $this->input->post('email');
+
+        $accountData = $this->Account->emailExists($account_email);
+
+        if(!empty($accountData)){
+            $to_email = $accountData;
+            $uData['apassword'] = rand('1111,9999').rand('a','z');
+            $updPassword = $this->Account->updatePassword($uData,$to_email);
+            //Load email library 
+            $this->load->library('email'); 
+
+            $this->email->from($from_email, 'SAD'); 
+            $this->email->to($to_email);
+            $this->email->subject('Recover Password'); 
+            $this->email->message('Your new password is "'.$new_password.'". Please use it to login.'); 
+
+            //Send mail 
+            if($this->email->send()){
+                //$this->session->set_flashdata("email_sent","Email sent successfully."); 
+                $response = [
+                    'status' => TRUE,
+                    'message' => 'Email sent successfully.',
+                    'response' => new stdClass()
+                ];
+
+                $this->set_response($response, REST_Controller::HTTP_CREATED); // CREATED (201) being the HTTP response code
+            } else {
+                //$this->session->set_flashdata("email_sent","Error in sending Email.");
+                // Set the response and exit
+                $this->set_response([
+                    'status' => FALSE,
+                    'message' => 'Error in sending Email. Please try again!',
+                    'response' => new stdClass()
+                ], REST_Controller::HTTP_OK); // NOT_FOUND (404) being the HTTP response code
+            }
+        }else{
+            // Set the response and exit
+            $this->set_response([
+                'status' => FALSE,
+                'message' => 'No users were found with this id!',
+                'response' => new stdClass()
+            ], REST_Controller::HTTP_OK); // NOT_FOUND (404) being the HTTP response code
+        }
+    }
+
+}
---
+++ application/models/Account.php
@@ -0,0 +1,113 @@
+<?php
+class Account extends CI_Model {
+	protected $CI;
+
+	function __construct() {
+		parent::__construct();
+		$this->CI =& get_instance();
+		//$this->load->model("Utility");
+	}
+
+	/**
+	* Add user's details
+	* return account id
+	*/
+	public function addAccount($accountData){
+		$db = $this->CI->db;
+		$db->trans_begin();
+		//Account Data Insert
+		foreach($accountData as $key => $val){
+			$db->set( $val['key'], $val['value'], $val['escape'] );	
+		}
+		$db->insert("accounts");
+		$account_id = $db->insert_id();
+
+		if ($db->trans_status() === FALSE){
+		    $db->trans_rollback();
+		    return false;
+		}else{
+		    $db->trans_commit();
+		    return $account_id;
+		}
+	}
+
+	public function emailExists($email){
+		$this->CI->db->select( "aemail" )->from( 'accounts' )->where( array( "aemail" => $email, "asocial" => "E" ) );
+		$query = $this->CI->db->get();
+		$row = $query->row();
+		//echo $this->CI->db->last_query(); die();
+		if ( $row ) return $row->aemail;
+	}
+
+	public function getAccount($account_id){
+		$base_url = base_url();
+		$base_url = 'http://192.168.0.198/sad/';
+		$this->CI->db->select( "id, aemail, afirstname, alastname, aprofile_photo" )->from('accounts')->where( "id", $account_id );
+		$query = $this->CI->db->get();
+		$row = $query->row();
+		if($row->aprofile_photo != ''){
+			$row->aprofile_photo = $base_url.'uploads/'.$row->aprofile_photo;
+		}
+		return $row;
+	}
+
+	public function getAccountField($account_id,$field){
+		$base_url = base_url();
+		$this->CI->db->select( $field )->from('accounts')->where( "id", $account_id );
+		$query = $this->CI->db->get();
+		$row = $query->row();
+		return $row;
+	}
+
+	public function getAccountAuthInformation($account_id){
+		$this->CI->db->select( "id, aemail, afirstname, alastname, apassword, asocial" )->from('accounts')->where( "id", $account_id );
+		$query = $this->CI->db->get();
+		$row = $query->row();
+		return $row;
+	}
+
+	public function accountExist($social_id){
+		$this->CI->db->select( "id" )->from('accounts')->where( "asocial_id", $social_id );
+		$query = $this->CI->db->get();
+		$row = $query->row();
+		//echo $this->CI->db->last_query(); die();
+		//if ( $row ) return $row->id;
+		if(isset($row->id) && $row->id > 0)
+			return $row->id;
+		return false;
+	}
+
+	public function accountLogin($aemail,$apassword){
+		$this->CI->db->select( "id, aemail, afirstname, alastname" )->from('accounts')->where( array( "aemail" => $aemail, "apassword" => md5($apassword) ) );
+		$query = $this->CI->db->get();
+		$row = $query->row();
+		//echo $this->CI->db->last_query(); die();
+		//if ( $row ) return $row->id;
+		if(isset($row->id) && $row->id > 0)
+			return $row;
+		return false;
+	}
+
+	public function updateAccount($data,$accid){
+		foreach ($data as $key => $value) {
+			$data[$key] = $value;
+		}
+		$db = $this->CI->db;
+		$db->update("accounts", $data, array( "id" => $accid ));
+		//echo $db->last_query(); die;
+		//return $db->affected_rows();
+		return true;
+	}
+
+	public function updatePassword($data,$email){
+		foreach ($data as $key => $value) {
+			$data[$key] = $value;
+		}
+		$db = $this->CI->db;
+		$db->update("accounts", $data, array( "aemail" => $email ));
+		//echo $db->last_query(); die;
+		//return $db->affected_rows();
+		return true;
+	}
+
+}
