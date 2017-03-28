b7a180db87c269709bcfbbc6c2d1f1d7690d06e1
Raju Rana <raju.rana@appsbee.com>
2017-03-28 19:23:20 +0530

    initial commit

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://stopalldistractions.com/sad/';
+$config['base_url'] = 'http://stopalldistractions.com/sad';
 //$config['base_url'] = 'http://localhost/sad';
 
 /*
@@ -36,7 +36,7 @@ $config['base_url'] = 'http://stopalldistractions.com/sad/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = '';
+$config['index_page'] = 'index.php';
 
 /*
 |--------------------------------------------------------------------------
