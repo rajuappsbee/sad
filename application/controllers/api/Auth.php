c1837b5abc34ce93d88361eb2628993cf2f05c00
Raju Rana <raju.rana@appsbee.com>
2017-03-31 18:50:37 +0530

    bug fixes

--- application/controllers/api/Auth.php
+++ application/controllers/api/Auth.php
@@ -494,7 +494,7 @@ class Auth extends REST_Controller {
 
         if(!empty($accountData)){
             $to_email = $accountData;
-            $new_password = rand('11111,99999');
+            $new_password = rand(11111,99999);
             $uData['apassword'] = md5($new_password);
             $updPassword = $this->Account->updatePassword($uData,$to_email);
             //Load email library 
